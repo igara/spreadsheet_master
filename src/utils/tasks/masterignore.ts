944a058332cefca6098c0c6bd890169b84d6b91c
garasyo <igara1119@gmail.com>
2020-02-23 18:39:36 +0900

    add: master ignore.

---
+++ src/utils/tasks/masterignore.ts
@@ -0,0 +1,13 @@
+import * as fs from "fs";
+const masterignoreFilePath = "data/.masterignore";
+
+export const read = () => {
+  try {
+    const masterignoreBuffer = fs.readFileSync(masterignoreFilePath);
+    const masterignoreString = masterignoreBuffer.toString();
+    const masterignore = masterignoreString.split("\n");
+    return masterignore;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
